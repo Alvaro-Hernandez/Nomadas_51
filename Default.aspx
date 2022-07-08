0b59203f95649dee99f41464bb781504279a6339
Alvaro-Hernandez <alvaropineda606@gmail.com>
2022-07-08 13:49:40 -0600

    Reporte Creado por Ruddy

---
+++ Default.aspx
@@ -0,0 +1,21 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true"  CodeFile="Default.aspx.cs" Inherits="_Default" %>
+<%@ Register TagPrefix="CR" Namespace="CrystalDecisions.Web" Assembly="CrystalDecisions.Web, Version=13.0.4000.0, Culture=neutral, PublicKeyToken=692fbea5521e1304" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <CR:CrystalReportViewer ID="CrystalReportViewer1" runat="server" AutoDataBind="True"
+            Height="1039px" ReportSourceID="CrystalReportSource1" Width="901px" />
+        <CR:CrystalReportSource ID="CrystalReportSource1" runat="server">
+            <Report FileName="CrystalReport1.rpt">
+            </Report>
+        </CR:CrystalReportSource>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+
+using CrystalDecisions.Shared;
+using CrystalDecisions.CrystalReports.Engine;
+using CrystalDecisions.Web;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page 
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SqlConnection cn = new SqlConnection(
+            @"Data Source=ALVARO-HERNANDE\ALVAROHERNANDEZ;
+            Initial Catalog=NomadasDB;Integrated Security=True;"
+        );
+
+        SqlCommand cmd = new SqlCommand("Select * FROM tb_habitacion;SELECT * FROM tb_negocio", cn);
+        SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+        DataSet ds = new DataSet();
+        adapter.Fill(ds);
+
+        ReportDocument rp = new ReportDocument();
+        rp.Load(Server.MapPath("ReporteHabitacion.rpt"));
+        rp.SetDataSource(ds.Tables["table"]);
+        rp.Subreports[0].SetDataSource(ds.Tables["table1"]);
+
+        rp.SetParameterValue("Renta", int.Parse(Request["Costo"]));
+        CrystalReportViewer1.ReportSource = rp;
+        rp.ExportToHttpResponse(ExportFormatType.PortableDocFormat, Response, false, "Informe de habitaciones");
+        Response.End();
+    }
+}
